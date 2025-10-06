24c24f49104d689902185388296b4a56812822d5
AIXpert22 <satvik.algorithmx@gmail.com>
2025-10-06 11:52:00 +0530

    Add files via upload

---
+++ Templates/sow_template.py
@@ -0,0 +1,29 @@
+from Examples.Scope_for_BRD import scope_example
+
+sow_template = f"""
+SOW(Scope of work)
+Purpose: Defines what will be delivered, timelines, resources, payment terms, and expectations. Often used in consulting/service contracts.
+
+Format:
+
+Introduction
+Project Overview
+Client and Vendor Names
+Objectives
+Purpose of the engagement
+Deliverables (e.g. Mobile app (Android), Web App)
+Scope of Work (Should be very well defined and explained based on deliverables and expanded in this format- {scope_example})
+Description of Work to Be Performed
+Week-wise or Month-wise Plan
+Team / Roles (Should be according to the key deliverables)
+Tech Stack 
+Client responsibilities
+Vendor responsibilities
+Assumptions
+Design to be shared by client
+Data will be provided by X date
+Out of Scope
+Hosting, SEO, etc.
+Change Request Process
+Sign-off
+"""
